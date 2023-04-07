b92528294bcb8e549ce1e11f11bd55b61c862b11
mohammad reza hassani <mrezahassani@protonmail.com>
2023-04-08 02:57:39 +0330

    initialize logger middlerware.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use newsletter::{configuration::get_configuration, run};
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // refactor database stuff later
     let configuration = get_configuration().expect("Failed to read configuration.");
     let pool = PgPool::connect(&configuration.database.connection_string())
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 
 pub mod configuration;
@@ -11,6 +11,7 @@ pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(pool.clone())
